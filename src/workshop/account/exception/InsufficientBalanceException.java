80849c12a8a9d8d33d453847e49ed7a38daa8a91
DARAM <merj4871@naver.com>
2026-08-25 16:45:03 +0900

    getCurrentBalance 정의

--- src/workshop/account/exception/InsufficientBalanceException.java
+++ src/workshop/account/exception/InsufficientBalanceException.java
@@ -1,5 +1,12 @@
 package workshop.account.exception;
 
-public class InsufficientBalanceException {
+//잔액이 부족할 때 예외처리
 
+public class InsufficientBalanceException extends Exception{
+	
+	private int currentBalance;
+	
+	public InsufficientBalanceException(String errorMessage) {
+		super(errorMessage);
+	}
 }
