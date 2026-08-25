a1d158183c9d8d615b80195e315752d43215d725
DARAM <merj4871@naver.com>
2026-08-25 15:22:50 +0900

    Account main

--- src/workshop/account/control/AccountTest.java
+++ src/workshop/account/control/AccountTest.java
@@ -1,7 +1,16 @@
 package workshop.account.control;
 
+import workshop.account.entity.*;
+
 public class AccountTest {
 	public static void main(String[] args) {
-		
+	Account account = new Account();	
+	
+	//고객번호 : “A1100”, 계좌번호 : “221-22-3477”, 잔액 : 100000
+	account.setCustId(“A1100”);
+	account.setAcctId(“221-22-3477”);
+	account.deposit(100000);
+	
+	System.out.println();
 	}
 }
