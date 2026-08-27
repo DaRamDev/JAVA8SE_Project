ba188d9c025d9b4c3be48c702811d86b09881f06
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-27 16:31:30 +0900

    HelloBeanSpingTest update

--- src/test/java/myspring/di/xml/HelloBeanSpingTest.java
+++ src/test/java/myspring/di/xml/HelloBeanSpingTest.java
@@ -1,9 +1,12 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-bean.xml")
 public class HelloBeanSpingTest {
-
+	
+	
 }
